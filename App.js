5415823077c28e8147ca4880fa96d9878c3800c6
Gustavo Alberto <gtilpe@gmail.com>
2022-05-01 11:51:16 -0300

    v2

--- App.js
+++ App.js
@@ -1,180 +1,258 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { StyleSheet, Text, View,StatusBar,SafeAreaView } from 'react-native';
 import styled from 'styled-components/native';
 import Display from './src/components/Display';
 
 export default function App() {
 
-  const [numeros,setNumeros] = useState('');
+  const [valorDisplay,setValorDisplay] = useState('');
+  const [memoria, setMemoria] = useState('');
   const [resultado, setResultado] =  useState('0');
+  const [display,setDisplay] = useState(); 
 
   function setNumero(numero) {
+    numero.toString();
+    if (vericarFormato(numero)) {
+      if (numero == '*') {
+        setValorDisplay(valorDisplay + "X");
+        setMemoria(memoria + numero);
+      } else {
+        setValorDisplay(valorDisplay + numero);
+        setMemoria(memoria + numero);
+      }
+    }
+  }
 
-    let  aux = numero.toString();
-    let  novoNumero = numeros + aux;
-    setNumeros(novoNumero)
+  function vericarFormato(numero) {
+      if (memoria == '' && numero == "+") {
+        return false;
+      } else if (memoria == '' && numero == "-") {
+        return false;
+      } else if (memoria == '' && numero == "/") {
+        return false;
+      }else if (memoria == '' && numero == "*") {
+        return false;
+      }else if (memoria == '' && numero == ".") {
+        return false;
+      }else{
+         return vericarUltimoDigito(numero);
+      }
+  }
+
+  function vericarUltimoDigito(numero) {
+    let ultimoDigito = memoria.substr(-1);
+
+    switch (ultimoDigito) {
+        case (ultimoDigito == "+" && numero == "+") :
+          return false;
+        break;
+        case ultimoDigito == "-" && numero == "-" :
+          return false;
+        break;
+        case ultimoDigito == "/" && numero == "/" :
+          return false;
+        break;
+        case ultimoDigito == "*" && numero == "*" :
+          return false;
+        break;
+        case ultimoDigito == "." && numero == "." :
+          return false;
+        break;
+        default : 
+          if (ultimoDigito == "+" && (numero == "+" || numero == "-" || numero == "*" || numero == "/" || numero == ".")) {
+            return false; 
+          } else if (ultimoDigito == "-" && (numero == "+" || numero == "-" || numero == "*" || numero == "/"  || numero == ".")){
+            return false
+          } else if (ultimoDigito == "/" && (numero == "+" || numero == "-" || numero == "*" || numero == "/"  || numero == ".")){
+            return false;
+          } else if (ultimoDigito == "*" && (numero == "+" || numero == "-" || numero == "*" || numero == "/"  || numero == ".")){
+            return false
+          } else if (ultimoDigito == "." && (numero == "+" || numero == "-" || numero == "*" || numero == "/"  || numero == ".")){ 
+            return false;
+          } else {
+            return true;
+          }
+        break;
+    }
+
   }
 
   function calcular() {
-       let resultado = eval(numeros);
-       resultado = parseFloat(resultado);
-       setResultado(resultado);
+    let ultimoDigito = memoria.substr(-1);
+
+    if (!((ultimoDigito == "-") || (ultimoDigito == "+") || (ultimoDigito == "/") || (ultimoDigito == "*"))){
+      if (memoria != '') {
+        let resultado = eval(memoria);
+        resultado = parseFloat(resultado);
+        setResultado(resultado);
+        setDisplay(false);
+      }
+    } 
   }
 
   function del() {
-     let novoNumero = numeros.substring(0, numeros.length - 1);
-     setNumeros(novoNumero);
+     setMemoria(memoria.substring(0, memoria.length - 1));
+     setValorDisplay(valorDisplay.substring(0, valorDisplay.length - 1));
   }
 
   function CC() {
-    setNumeros('');
+    setMemoria('');
+    setValorDisplay('');
     setResultado(0);
   }
 
+  useEffect(()=>{
+    calcular();
+  },[memoria]);
+ 
+
   return (
-    <Container>
-        <StatusBar 
-            style="auto" 
-            backgroundColor="blue"
-            hidden={true}/>
-        
-          <Display
-              numeros = {numeros}
-              resultado = {resultado}
-          />
-          <ContainerButton>
-              <AreaButtonNumber>
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(7)}>
-                      <Number>7</Number>
-                  </Botao>      
+    <SafeAreaView>
+      <Container>
+          <StatusBar 
+              style="auto" 
+              backgroundColor="blue"
+              hidden={true}/>
+          
+            <Display
+                valorDisplay = {valorDisplay}
+                resultado = {resultado}
+                display = {display}
+            />
+            <ContainerButton>
+                <AreaButtonNumber>
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(7)}>
+                        <Number>7</Number>
+                    </Botao>      
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(8)}>
-                      <Number>8</Number>
-                  </Botao> 
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(8)}>
+                        <Number>8</Number>
+                    </Botao> 
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(9)}>
-                      <Number>9</Number>
-                  </Botao> 
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(9)}>
+                        <Number>9</Number>
+                    </Botao> 
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() =>setNumero(4)}>
-                      <Number>4</Number>
-                  </Botao> 
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() =>setNumero(4)}>
+                        <Number>4</Number>
+                    </Botao> 
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(5)}>
-                      <Number>5</Number>
-                  </Botao> 
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(5)}>
+                        <Number>5</Number>
+                    </Botao> 
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(6)}>
-                      <Number>6</Number>
-                  </Botao> 
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(6)}>
+                        <Number>6</Number>
+                    </Botao> 
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(1)}>
-                      <Number>1</Number>
-                  </Botao> 
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(1)}>
+                        <Number>1</Number>
+                    </Botao> 
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(2)}>
-                      <Number>2</Number>
-                  </Botao> 
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(2)}>
+                        <Number>2</Number>
+                    </Botao> 
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(3)}>
-                      <Number>3</Number>
-                  </Botao>       
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(3)}>
+                        <Number>3</Number>
+                    </Botao>       
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() =>del()}>
-                      <Number>del</Number>
-                  </Botao>  
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() =>del()}>
+                        <Number>del</Number>
+                    </Botao>  
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero(0)}>
-                      <Number>0</Number>
-                  </Botao>     
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero(0)}>
+                        <Number>0</Number>
+                    </Botao>     
 
-                  <Botao 
-                    activeOpacity={1}
-                    underlayColor="#b31c17"
-                    onPress={() => setNumero('.')}>
-                      <Number>.</Number>
-                  </Botao> 
+                    <Botao 
+                      activeOpacity={1}
+                      underlayColor="#b31c17"
+                      onPress={() => setNumero('.')}>
+                        <Number>.</Number>
+                    </Botao> 
 
-                  <BotaoCC
-                       activeOpacity={1}
-                       underlayColor="#b31c17"
-                       onPress={() => CC()}>
-                      <Number>CC</Number>
-                  </BotaoCC>
-              </AreaButtonNumber>
-
-              <AreaButtonOperatiion>
-                    <BotaoOperation 
+                    <BotaoCC
                         activeOpacity={1}
                         underlayColor="#b31c17"
-                        onPress={() => setNumero('/')}>
-                        <Number>/</Number>
-                    </BotaoOperation>   
+                        onPress={() => CC()}>
+                        <Number>AC</Number>
+                    </BotaoCC>
+                </AreaButtonNumber>
 
-                    <BotaoOperation 
-                        activeOpacity={1}
-                        underlayColor="#b31c17"
-                        onPress={() => setNumero('*')}>
-                        <Number>X</Number>
-                    </BotaoOperation>   
+                <AreaButtonOperatiion>
+                      <BotaoOperation 
+                          activeOpacity={1}
+                          underlayColor="#b31c17"
+                          onPress={() => setNumero('/')}>
+                          <Number>/</Number>
+                      </BotaoOperation>   
 
-                    <BotaoOperation 
-                        activeOpacity={1}
-                        underlayColor="#b31c17"
-                        onPress={() => setNumero('-')}>
-                        <Number>-</Number>
-                    </BotaoOperation>   
+                      <BotaoOperation 
+                          activeOpacity={1}
+                          underlayColor="#b31c17"
+                          onPress={() => setNumero('*')}>
+                          <Number>X</Number>
+                      </BotaoOperation>   
 
-                    <BotaoOperation 
-                        activeOpacity={1}
-                        underlayColor="#b31c17"
-                        onPress={() => setNumero('+')}>
-                        <Number>+</Number>
-                    </BotaoOperation> 
+                      <BotaoOperation 
+                          activeOpacity={1}
+                          underlayColor="#b31c17"
+                          onPress={() => setNumero('-')}>
+                          <Number>-</Number>
+                      </BotaoOperation>   
 
-                    <BotaoOperation 
-                        activeOpacity={1}
-                        underlayColor="#b31c17"
-                        onPress={() => calcular()}>
-                        <Number>=</Number>
-                    </BotaoOperation>     
-                    
-              </AreaButtonOperatiion>
-          </ContainerButton>
-    </Container>
+                      <BotaoOperation 
+                          activeOpacity={1}
+                          underlayColor="#b31c17"
+                          onPress={() => setNumero('+')}>
+                          <Number>+</Number>
+                      </BotaoOperation> 
+
+                      <BotaoOperation 
+                          activeOpacity={1}
+                          underlayColor="#b31c17"
+                          onPress={() => setDisplay(true)}>
+                          <Number>=</Number>
+                      </BotaoOperation>     
+                      
+                </AreaButtonOperatiion>
+            </ContainerButton>
+      </Container>
+    </SafeAreaView>
   );
 }
 
@@ -197,12 +275,12 @@ const AreaButtonNumber = styled.View`
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
-    margin-top: 10px;
+    margin-top: 1px;
 `;
 
 const AreaButtonOperatiion = styled.View`
    width: 25%;
-   margin-top: 10px;
+   margin-top: 1px;
    padding-right: 10px
 `;
 
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,38 +1,52 @@
-import react from "react";
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
 import { StyleSheet, Text, View,StatusBar } from 'react-native';
 
 export default (props) => {
+
+    const [displayDigitos, setDisplayDigitos] = useState();
     return(
         <Display>
-            <Numeros>
-                {props.numeros}
-            </Numeros>
-            
-            <Resultado>
-               {(props.resultado == '0') ? '' : '='} {props.resultado}
-            </Resultado>
+            {props.display && 
+                 <Resultado>
+                    {props.resultado}
+                 </Resultado>
+            } 
+
+            {!props.display && 
+                <>
+                    <Numeros>
+                    {props.valorDisplay}
+                    </Numeros>
+                    
+                    <Resultado>
+                    {(props.resultado == '0') ? '' : '='} {props.resultado}
+                    </Resultado>
+                </>
+            } 
+           
         </Display>
     );
 }
 
 const Display = styled.View`
    width: 90%;
-   height: 250px;
+   height: 28%;
    margin: 30px auto;
    border-radius: 20px;
    justify-content: center;
 `;
 
-const Numeros = styled.Text`
+const Numeros = styled.TextInput`
     color: #fff; 
-    width: 100%; 
+    width: 100%;
     font-size: 40px;
     text-align: right;
+    flex:1;
 `
 
 
-const Resultado = styled.Text`
+const Resultado = styled.TextInput`
     color: #fff; 
     width: 100%; 
     font-size: 60px;
